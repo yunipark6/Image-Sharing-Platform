02e766ee8d43bd65c6dd24a34a3773c96f7baa24
Yuni Park <yunip@umich.edu>
2023-03-22 11:09:34 -0400

    some UI added on login page serverside

--- serverside-dynamic-webapp/insta485/__init__.py
+++ serverside-dynamic-webapp/insta485/__init__.py
@@ -18,6 +18,6 @@ app.config.from_envvar('INSTA485_SETTINGS', silent=True)
 # Tell our app about views and model.  This is dangerously close to a
 # circular import, which is naughty, but Flask was designed that way.
 # (Reference http://flask.pocoo.org/docs/patterns/packages/)  We're
-# going to tell pylint and pycodestyle to ignore this coding style violation.
+
 import insta485.views  # noqa: E402  pylint: disable=wrong-import-position
 import insta485.model  # noqa: E402  pylint: disable=wrong-import-position
